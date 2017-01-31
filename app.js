f80a3e3c62a259725ccd8afb609e00ac6463ae32
troywingert21 <trkla@email.sc.edu>
2017-01-31 11:48:51 -0500

    Added server startup

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 var login = require("facebook-chat-api");
 var fs = require('fs');
 var prompt = require ('prompt');
+var io  = require('socket.io').listen(5001),
+    dl  = require('delivery'),
+    fs  = require('fs');
 
 prompt.start();
 prompt.get(['email', 'password'], function (err, result) {
   console.log('Login Successful!');
 
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Test\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 login({email: result.email, password: result.password}, function callback (err, api) {
     if(err) return console.error(err);
@@ -49,4 +66,61 @@ login({email: result.email, password: result.password}, function callback (err, 
         }
     });
 });
+var delivery = dl.listen(socket);
+var socket = io.connect('http//127.0.0.1:3000');
+
+socket.on( 'connect', function() {
+  log( "Sockets connected" );
+        
+  delivery = dl.listen( socket );
+  delivery.connect();
+    
+  delivery.on('delivery.connect',function(delivery){
+    delivery.send({
+      name: 'myfile.txt',
+      path : './myfile.txt'
+    });
+ 
+    delivery.on('send.success',function(file){
+      console.log('File sent successfully!');
+    });
+  });  
 });
+});
+/*
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Test\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});*/
+
+/*
+var delivery = dl.listen(socket);
+var socket = io.connect('http//127.0.0.1:3000');
+
+socket.on( 'connect', function() {
+  log( "Sockets connected" );
+        
+  delivery = dl.listen( socket );
+  delivery.connect();
+    
+  delivery.on('delivery.connect',function(delivery){
+    delivery.send({
+      name: 'myfile.txt',
+      path : './myfile.txt'
+    });
+ 
+    delivery.on('send.success',function(file){
+      console.log('File sent successfully!');
+    });
+  });  
+});*/
