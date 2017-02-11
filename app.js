4e0985f5d4d6bf9cf2b486fc249b151da6f21c79
troywingert21 <trkla@email.sc.edu>
2017-02-10 21:56:33 -0500

    Cleaning up code

--- app.js
+++ app.js
@@ -51,19 +51,6 @@ login({email: 'tornadotroy8@yahoo.com', password: 'associate1808346'}, function 
             
             chatHistory(0, 99, event.threadID);
 
-            /*api.getThreadHistory(event.threadID, 0, 99, null, function(err, history){
-            	if (err) throw err;
-
-            	for (var j = history.length - 2; j >= 0; j--){
-            		//console.log(history[j].body);
-            		console.log(history[j]);
-            		//output(api, event, history[j], save);
-            	}
-              console.log(event.body);
-
-            })*/
-
-            //while (event.body != '/stop' || )
             fs.writeFile('myfile.txt',event.body, (err) => {
               if(err) throw err;
                 console.log('It\'s saved!');
@@ -90,54 +77,6 @@ login({email: 'tornadotroy8@yahoo.com', password: 'associate1808346'}, function 
             })
     }
 });
-
-
-    /*function output(api, message, text, save) {
-    	v.section = 'quote output';
-    	var tag = '-' + text.senderName + ' ' + moment(text.timestamp).format('MM/DD/YYYY');
-    	var s = text.body;
-    	if (!v.contains(text.body, tag)) s += '\n' + tag;
-    	api.sendMessage(s, message.threadID);
-    	if (save) f.setDataSimple('threads/' + message.threadID + '/quotes/' + Date.now() + '_' + message.senderID, s, null);
-}*/
-//});
-
-/*var delivery = dl.listen(socket);
-var socket = io.connect('http//127.0.0.1:3000');
-
-socket.on( 'connect', function() {
-  log( "Sockets connected" );
-        
-  delivery = dl.listen( socket );
-  delivery.connect();
-    
-  delivery.on('delivery.connect',function(delivery){
-    delivery.send({
-      name: 'myfile.txt',
-      path : './myfile.txt'
-    });
- 
-    delivery.on('send.success',function(file){
-      console.log('File sent successfully!');
-    });
-  });  
-});*/
-
-/*
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Test\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});*/
 
 /*
 var delivery = dl.listen(socket);
