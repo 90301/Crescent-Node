d84d51c6eada6486f3b25b66181a516bbcacca2c
troywingert21 <trkla@email.sc.edu>
2017-02-10 21:38:28 -0500

    Message History

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ var prompt = require ('prompt');
 var io  = require('socket.io').listen(5001),
     dl  = require('delivery'),
     fs  = require('fs');
+var v = require('globals');
 
-prompt.start();
-prompt.get(['email', 'password'], function (err, result) {
-  console.log('Login Successful!');
+global.name = 'v';
+global.name = 'f';
+
+//prompt.start();
+//prompt.get(['email', 'password'], function (err, result) {
+//  console.log('Login Successful!');
 
 const http = require('http');
 
@@ -23,16 +27,15 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-login({email: result.email, password: result.password}, function callback (err, api) {
+//{email: result.email, password: result.password}
+login({email: 'tornadotroy8@yahoo.com', password: 'associate1808346'}, function callback (err, api) {
     if(err) return console.error(err);
 
     api.setOptions({
       logLevel: "silent"
     });
 
-
-    var stopListening = api.listen(function(err, event) {
+    var stopListening = api.listen(function(err, event, save) {
         if(err) return console.error(err);
 
         switch(event.type) {
@@ -47,8 +50,16 @@ login({email: result.email, password: result.password}, function callback (err, 
             console.log(event.body, event.threadID);
             
 
-            api.getThreadHistory(event.threadID, 0, 5, new Date().getTime(), function(err, data){
+            api.getThreadHistory(event.threadID, 0, 99, null, function(err, history){
+            	if (err) throw err;
 
+            	console.log('In function');
+
+            	for (var j = history.length - 2; j >= 0; j--){
+            		//console.log(history[j].body);
+            		console.log(history[j]);
+            		//output(api, event, history[j], save);
+            	}
               console.log(event.body);
 
             })
@@ -65,8 +76,19 @@ login({email: result.email, password: result.password}, function callback (err, 
             break;
         }
     });
+
+    /*function output(api, message, text, save) {
+    	v.section = 'quote output';
+    	var tag = '-' + text.senderName + ' ' + moment(text.timestamp).format('MM/DD/YYYY');
+    	var s = text.body;
+    	if (!v.contains(text.body, tag)) s += '\n' + tag;
+    	api.sendMessage(s, message.threadID);
+    	if (save) f.setDataSimple('threads/' + message.threadID + '/quotes/' + Date.now() + '_' + message.senderID, s, null);
+}*/
 });
-var delivery = dl.listen(socket);
+//});
+
+/*var delivery = dl.listen(socket);
 var socket = io.connect('http//127.0.0.1:3000');
 
 socket.on( 'connect', function() {
@@ -85,8 +107,8 @@ socket.on( 'connect', function() {
       console.log('File sent successfully!');
     });
   });  
-});
-});
+});*/
+
 /*
 const http = require('http');
 
